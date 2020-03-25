adac824ac79a1b192b330c9d3d3265bd109a3d72
AndreyBocharnikov <a-bocharnikov@bk.ru>
2020-03-25 17:29:54 +0300

    test checking added

--- hw3/task2.m
+++ hw3/task2.m
@@ -1,6 +1,25 @@
 pkg load statistics
 
-n = 100;
+function p = test(n, a, b)
+  m = 100;
+  X = unifrnd(a, b, m, n);
+  X = sort(X);
+  X_1 = X(1, :);
+  F_X_1 = unifcdf(X_1, a, b);
+  res = max(abs(F_X_1  - 1 / n), F_X_1);
+  for i=2:m
+    #printf("%d %d\n", edf_x(1, i), unifcdf(edf_x(1, i), a, b));
+    X_i = X(i, :);
+    F_X_i = unifcdf(X_i, a, b);
+    current_val = max(abs(F_X_i  - i / m), abs(F_X_i - (i - 1) / m));
+    res = max(res, current_val);
+  endfor
+  gamma = 0.95;
+  u_gamma = 1.36;
+  p = mean((sqrt(m) * res) > u_gamma)
+endfunction 
+
+n = 10;
 a = 20;
 b = 80;
 
@@ -9,6 +28,9 @@ df_y = unifcdf(df_x, a, b);
 
 edf_x = sort(unifrnd(a, b, 1, n));
 edf_y = 1/n:1/n:1;
+
+test(10000, a, b);
+test(1000000, a, b);
 
 [st_a, st_b] = stairs(edf_x, edf_y);
 
