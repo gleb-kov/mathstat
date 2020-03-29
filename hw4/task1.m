43e91f7d9490e8c3d90e06f5a9ab434c9d04a653
Alexey Shishkin <dr.alexa2000@mail.ru>
2020-03-29 17:47:21 +0300

    hw4 is ready

--- hw4/task1.m
+++ hw4/task1.m
@@ -1,8 +1,21 @@
 pkg load statistics
 
+clc;
+clear all;
+
+function res = f(x)
+  res = exp(- x ^ 2 / 2);
+endfunction
+
+function res = laplace(x)
+  res = quad(@f, 0, x) * 1 / sqrt(2 * pi);
+endfunction
+
 n = 10 ^ 5;
 mu = 1;
 sigma = 1;
+alpha = 0.95;
+real_hi2 = 100734.7;
 
 X = sort(normrnd(mu, sigma, n, 1));
 
@@ -30,3 +43,35 @@ printf("Размер выборки = %d\n", n);
 printf("Границы = [%d; %d]\n", l, r);
 printf("Выбранная длина интервалов = %d\n", delta);
 printf("Количество интервалов = %d\n", buckets);
+
+printf("\n");
+
+# PART 2
+
+E = sum(x_coords .* cnt_in_bucket) / n;
+D = sum((x_coords - E) .^ 2 .* cnt_in_bucket) / n;
+FIXED_D = n / (n - 1) * D;
+SQ_D = sqrt(FIXED_D);
+
+printf("Предполагаемое матожидание = %d\n", E);
+printf("Предполагаемая дисперсия = %d\n", FIXED_D);
+
+# suffix P means "Предполагаемое"
+
+z = (walls - E) / SQ_D;
+
+P = zeros(buckets, 1);
+
+P(1) = laplace(z(1)) - (-0.5);
+for i = 2 : buckets
+  P(i) = laplace(z(i)) - laplace(z(i - 1));
+endfor
+
+nP = P * n;
+
+hi2 = sum(((cnt_in_bucket - nP) .^ 2) ./ nP);
+
+printf("Полученное значение Хи квадрат = %d\n", hi2);
+printf("Теоретическое значение Хи квадрат = %d\n", real_hi2);
+printf("Заданное распределение есть равномерное = %d\n", hi2 < real_hi2);
+
