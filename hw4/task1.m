4f3c909681cb0f48629524bd453226594d4e11ee
Alexey Shishkin <dr.alexa2000@mail.ru>
2020-03-29 13:34:47 +0300

    TASK 4 PART 1 COMPLETED

--- hw4/task1.m
+++ hw4/task1.m
@@ -1,2 +1,23 @@
 pkg load statistics
 
+n = 4 * 10 ^ 4;
+buckets = n / 10;
+mu = 1;
+sigma = 1;
+
+X = normrnd(mu, sigma, n, 1);
+
+l = min(X);
+r = max(X);
+delta = (r - l) / buckets;
+x_coords = zeros(buckets, 1);
+y_coords = zeros(buckets, 1);
+
+for i=1:buckets
+  cur_l = i * delta + l;
+  cur_r = cur_l + delta;
+  x_coords(i) = (cur_r + cur_l) / 2;
+  y_coords(i) = (sum(X <= cur_r) - sum(X < cur_l)) / (n * delta);
+endfor
+
+bar(x_coords, y_coords);
