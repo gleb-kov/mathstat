56fb0c217dfacff873dfe0f8d2ea9725fc92a352
Alexey Shishkin <dr.alexa2000@mail.ru>
2020-04-13 10:27:58 +0300

    hw4 fix

--- hw4/task1.m
+++ hw4/task1.m
@@ -3,7 +3,8 @@ pkg load statistics
 clc;
 clear all;
 
-function res = test_Chi2(tests, n, m, fix)
+
+function res = test_Chi2_1(tests, n, m)
   res = 0;
   for t = 1 : tests
     mu = 1;
@@ -26,25 +27,74 @@ function res = test_Chi2(tests, n, m, fix)
       cnt_in_bucket(i) = sum(X <= cur_r) - sum(X < cur_l);
     endfor
 
-    if (fix == 1)
-      fixed_cnt_in_bucket = zeros(m, 1);
-      fixed_walls = zeros(m, 2);
-      cur = 1;
-      for i = 1 : m
-        if (i == 1 || fixed_cnt_in_bucket(cur) >= 6)
-          cur = cur + 1;
-          fixed_walls(cur, 1) = walls(i, 1);
-        endif
-        fixed_walls(cur, 2) = walls(i, 2);
-        fixed_cnt_in_bucket(cur) = fixed_cnt_in_bucket(cur) + cnt_in_bucket(i);
-      endfor
-      m = cur;
-      walls = fixed_walls;
-      cnt_in_bucket = fixed_cnt_in_bucket;
-      resize(walls, m, 1);
-      resize(cnt_in_bucket, m, 1);
-      printf("После исправления m = %d\n", m);
-    endif
+    x_coords = zeros(m, 1);
+    for i = 1 : m
+      x_coords(i) = (walls(i, 2) + walls(i, 1)) / 2;
+    endfor
+    
+    #Выборочное среднее
+    E = sum(x_coords .* cnt_in_bucket) / n;
+    #Выборочная дисперсия
+    D = sum((x_coords - E) .^ 2 .* cnt_in_bucket) / n;
+    SQRT_D = sqrt(D);
+
+    P = zeros(m, 1);
+
+    for i = 1 : m
+      cur_l = walls(i, 1);
+      cur_r = walls(i, 2);
+      P(i) = normcdf(cur_r, E, SQRT_D) - normcdf(cur_l, E, SQRT_D);
+    endfor
+
+    hi2 = sum(((cnt_in_bucket - n .* P) .^ 2) ./ (n .* P));
+    # TODO: исправить подсчет значения chi2inv
+    res = res + (hi2 < chi2inv(1 - alpha, m - 1));
+  endfor
+  printf("Успешно %d из %d\n", res, tests);
+endfunction
+
+function res = test_Chi2_2(tests, n, m)
+  res = 0;
+  for t = 1 : tests
+    mu = 1;
+    sigma = 1;
+    alpha = 0.95;
+
+    X = sort(normrnd(mu, sigma, n, 1));
+
+    l = min(X);
+    r = max(X);
+    
+    delta = (r - l) / m;
+    cnt_in_bucket = zeros(m, 1);
+    walls = zeros(m, 2);
+    for i = 1 : m
+      cur_l = (i - 1) * delta + l;
+      cur_r = cur_l + delta;
+      walls(i, 1) = cur_l;
+      walls(i, 2) = cur_r;
+      cnt_in_bucket(i) = sum(X <= cur_r) - sum(X < cur_l);
+    endfor
+
+    fixed_cnt_in_bucket = zeros(m, 1);
+    fixed_walls = zeros(m, 2);
+    cur = 0;
+    for i = 1 : m
+      if (i == 1 || fixed_cnt_in_bucket(cur) >= 6)
+        cur = cur + 1;
+        fixed_walls(cur, 1) = walls(i, 1);
+      endif
+      fixed_walls(cur, 2) = walls(i, 2);
+      fixed_cnt_in_bucket(cur) = fixed_cnt_in_bucket(cur) + cnt_in_bucket(i);
+    endfor
+    m = cur;
+    walls = zeros(m, 1);
+    cnt_in_bucket = zeros(m, 1);
+    for i = 1 : m
+      walls(i, 1) = fixed_walls(i, 1);
+      walls(i, 2) = fixed_walls(i, 2);
+      cnt_in_bucket(i) = fixed_cnt_in_bucket(i);
+    endfor
     
     x_coords = zeros(m, 1);
     for i = 1 : m
@@ -109,8 +159,8 @@ printf("\n");
 
 # PART 2
 
-test_Chi2(10 ^ 3, 10 ^ 4, m, 0);
+test_Chi2_1(10 ^ 2, 10 ^ 4, m);
 
 # nj >= 6
-test_Chi2(10 ^ 3, 10 ^ 4, m, 1);
+test_Chi2_2(10 ^ 2, 10 ^ 4, m);
 
