ba53a708675a431f80b9261a974a6bd2e6735209
Alexey Shishkin <dr.alexa2000@mail.ru>
2020-04-13 17:07:49 +0300

    fixed hw4

--- hw4/task1.m
+++ hw4/task1.m
@@ -15,39 +15,30 @@ function res = test_Chi2_1(tests, n, m)
     r = max(X);
     
     delta = (r - l) / m;
-    cnt_in_bucket = zeros(m, 1);
-    walls = zeros(m, 2);
+    cnt_in_bucket = hist(X, m);
+    walls = [];
+    x_coords = [];
     for i = 1 : m
-      cur_l = (i - 1) * delta + l;
-      cur_r = cur_l + delta;
-      walls(i, 1) = cur_l;
-      walls(i, 2) = cur_r;
-      cnt_in_bucket(i) = sum(X <= cur_r) - sum(X < cur_l);
-    endfor
-
-    x_coords = zeros(m, 1);
-    for i = 1 : m
+      walls(i, 1) = l + delta * (i - 1);
+      walls(i, 2) = l + delta * i;
       x_coords(i) = (walls(i, 2) + walls(i, 1)) / 2;
     endfor
-    
+
     #Выборочное среднее
     E = sum(x_coords .* cnt_in_bucket) / n;
     #Выборочная дисперсия
     D = sum((x_coords - E) .^ 2 .* cnt_in_bucket) / n;
     SQRT_D = sqrt(D);
 
-    P = zeros(m, 1);
-
+    P = [];
     for i = 1 : m
-      cur_l = walls(i, 1);
-      cur_r = walls(i, 2);
-      P(i) = normcdf(cur_r, E, SQRT_D) - normcdf(cur_l, E, SQRT_D);
+      P(i) = normcdf(walls(i, 2), E, SQRT_D) - normcdf(walls(i, 1), E, SQRT_D);
     endfor
 
     hi2 = sum(((cnt_in_bucket - n .* P) .^ 2) ./ (n .* P));
-    # TODO: исправить подсчет значения chi2inv
-    res = res + (hi2 < chi2inv(1 - alpha, m - 1));
+    res = res + (hi2 < chi2inv(1 - alpha, m - 1 - 2));
   endfor
+  printf("Нормальное распределение проходит проверку гипотезы о нормальном распределении\n");
   printf("Для alpha = %d, успешно %d из %d\n", alpha, res, tests);
 endfunction
 
@@ -63,39 +54,73 @@ function res = test_Chi2_2(tests, n, m)
     r = max(X);
     
     delta = (r - l) / m;
-    cnt_in_bucket = zeros(m, 1);
-    walls = zeros(m, 2);
+    cnt_in_bucket = hist(X, m);
+    x_coords = [];
+    walls = [];
     for i = 1 : m
       cur_l = (i - 1) * delta + l;
       cur_r = cur_l + delta;
       walls(i, 1) = cur_l;
       walls(i, 2) = cur_r;
-      cnt_in_bucket(i) = sum(X <= cur_r) - sum(X < cur_l);
+      x_coords(i) = (cur_r + cur_l) / 2;
     endfor
 
-    fixed_cnt_in_bucket = zeros(m, 1);
-    fixed_walls = zeros(m, 2);
-    cur = 0;
+    fixed_cnt_in_bucket = [];
+    fixed_walls = [];
+    fixed_x_coords = [];
+    fixed_m = 0;
     for i = 1 : m
-      if (i == 1 || fixed_cnt_in_bucket(cur) >= 6)
-        cur = cur + 1;
-        fixed_walls(cur, 1) = walls(i, 1);
+      if (i == 1 || fixed_cnt_in_bucket(fixed_m) >= 6)
+        fixed_m = fixed_m + 1;
+        fixed_walls(fixed_m, 1) = walls(i, 1);
+        fixed_cnt_in_bucket(fixed_m) = 0;
+        fixed_x_coords(fixed_m) = 0;
       endif
-      fixed_walls(cur, 2) = walls(i, 2);
-      fixed_cnt_in_bucket(cur) = fixed_cnt_in_bucket(cur) + cnt_in_bucket(i);
-    endfor
-    m = cur;
-    walls = zeros(m, 1);
-    cnt_in_bucket = zeros(m, 1);
-    for i = 1 : m
-      walls(i, 1) = fixed_walls(i, 1);
-      walls(i, 2) = fixed_walls(i, 2);
-      cnt_in_bucket(i) = fixed_cnt_in_bucket(i);
+      fixed_walls(fixed_m, 2) = walls(i, 2);
+      fixed_cnt_in_bucket(fixed_m) = fixed_cnt_in_bucket(fixed_m) + cnt_in_bucket(i);
+      fixed_x_coords(fixed_m) = fixed_x_coords(fixed_m) + cnt_in_bucket(i) * x_coords(i);
     endfor
     
-    x_coords = zeros(m, 1);
+    fixed_x_coords = fixed_x_coords ./ fixed_cnt_in_bucket;
+    
+    #Выборочное среднее
+    E = sum(fixed_x_coords .* fixed_cnt_in_bucket) / n;
+    #Выборочная дисперсия
+    D = sum((fixed_x_coords - E) .^ 2 .* fixed_cnt_in_bucket) / n;
+    SQRT_D = sqrt(D);
+
+    P = [];
+    for i = 1 : fixed_m
+      P(i) = normcdf(fixed_walls(i, 2), E, SQRT_D) - normcdf(fixed_walls(i, 1), E, SQRT_D);
+    endfor
+
+    hi2 = sum(((fixed_cnt_in_bucket - n .* P) .^ 2) ./ (n .* P));
+    res = res + (hi2 < chi2inv(1 - alpha, fixed_m - 1 - 2));
+  endfor
+  printf("Нормальное распределение проходит проверку гипотезы о нормальном распределении\n");
+  printf("Данные сгруппированы, чтобы выполнялось nj >= 6\n");
+  printf("Для alpha = %d, успешно %d из %d\n", alpha, res, tests);
+endfunction
+
+function res = test_Chi2_3(tests, n, m)
+  res = 0;
+  mu = 1;
+  sigma = 1;
+  alpha = 0.05;
+  for t = 1 : tests
+    X = sort(normrnd(mu, sigma, n, 1));
+
+    l = min(X);
+    r = max(X);
+    
+    delta = (r - l) / m;
+    cnt_in_bucket = hist(X, m);
+    x_coords = [];
+    walls = [];
     for i = 1 : m
-      x_coords(i) = (walls(i, 2) + walls(i, 1)) / 2;
+      walls(i, 1) = l + delta * (i - 1);
+      walls(i, 2) = l + delta * i;
+      x_coords(i) = (walls(i, 1) + walls(i, 2)) / 2;
     endfor
     
     #Выборочное среднее
@@ -104,52 +129,44 @@ function res = test_Chi2_2(tests, n, m)
     D = sum((x_coords - E) .^ 2 .* cnt_in_bucket) / n;
     SQRT_D = sqrt(D);
 
-    P = zeros(m, 1);
-
+    P = [];
     for i = 1 : m
-      cur_l = walls(i, 1);
-      cur_r = walls(i, 2);
-      P(i) = normcdf(cur_r, E, SQRT_D) - normcdf(cur_l, E, SQRT_D);
+      P(i) = unifcdf(walls(i, 2), l, r) - unifcdf(walls(i, 1), l, r);
     endfor
 
     hi2 = sum(((cnt_in_bucket - n .* P) .^ 2) ./ (n .* P));
-    # TODO: исправить подсчет значения chi2inv
-    res = res + (hi2 < chi2inv(1 - alpha, m - 1));
+    res = res + (hi2 < chi2inv(1 - alpha, m - 1 - 2));
   endfor
-  printf("Данные сгруппированы, чтобы выполнялось nj >= 6\n");
+  printf("Нормальное распределение проходит проверку гипотезы о равномерном распределении\n");
   printf("Для alpha = %d, успешно %d из %d\n", alpha, res, tests);
 endfunction
 
 n = 10 ^ 6;
 mu = 1;
 sigma = 1;
-m = 100;
+m = 10 ^ 2;
+tests = 10 ^ 3;
 
 X = sort(normrnd(mu, sigma, n, 1));
 
 l = min(X);
 r = max(X);
 delta = (r - l) / m;
-x_coords = zeros(m, 1);
-cnt_in_bucket = zeros(m, 1);
-y_coords = zeros(m, 1);
+x_coords = [];
+y_coords = hist(X, m)' / (n * delta);
 
 for i = 1 : m
   cur_l = (i - 1) * delta + l;
   cur_r = cur_l + delta;
   x_coords(i) = (cur_r + cur_l) / 2;
-  cnt_in_bucket(i) = sum(X <= cur_r) - sum(X < cur_l);
-  y_coords(i) = cnt_in_bucket(i) / (n * delta);
 endfor
 
-# TODO: научиться строить график
 x_coords_for_normpdf = l:0.1:r;
 stairs(x_coords, y_coords);
 hold on;
 plot(x_coords_for_normpdf, normpdf(x_coords_for_normpdf, mu, sigma));
 
 printf("Размер выборки = %d\n", n);
-printf("Границы = [%d; %d]\n", l, r);
 printf("Выбранная длина интервалов = %d\n", delta);
 printf("Количество интервалов = %d\n", m);
 
@@ -157,10 +174,13 @@ printf("\n");
 
 # PART 2
 
-test_Chi2_1(10 ^ 3, 10 ^ 4, m);
+test_Chi2_1(tests, 10 ^ 4, m);
 
 printf("\n");
 
-# nj >= 6
-test_Chi2_2(10 ^ 3, 10 ^ 4, m);
+test_Chi2_2(tests, 10 ^ 4, m);
+
+printf("\n");
+
+test_Chi2_3(tests, 10 ^ 4, m);
 
--- hw4/task2.m
+++ hw4/task2.m
@@ -15,18 +15,12 @@ function res = test_Chi2_1(tests, n, m)
     r = max(X);
     
     delta = (r - l) / m;
-    cnt_in_bucket = zeros(m, 1);
-    walls = zeros(m, 2);
+    cnt_in_bucket = hist(X, m);
+    walls = [];
+    x_coords = [];
     for i = 1 : m
-      cur_l = (i - 1) * delta + l;
-      cur_r = cur_l + delta;
-      walls(i, 1) = cur_l;
-      walls(i, 2) = cur_r;
-      cnt_in_bucket(i) = sum(X <= cur_r) - sum(X < cur_l);
-    endfor
-
-    x_coords = zeros(m, 1);
-    for i = 1 : m
+      walls(i, 1) = l + (i - 1) * delta;
+      walls(i, 2) = l + i * delta;
       x_coords(i) = (walls(i, 2) + walls(i, 1)) / 2;
     endfor
     
@@ -36,44 +30,77 @@ function res = test_Chi2_1(tests, n, m)
     D = sum((x_coords - E) .^ 2 .* cnt_in_bucket) / n;
     SQRT_D = sqrt(D);
 
-    P = zeros(m, 1);
-
+    P = [];
     for i = 1 : m
-      cur_l = walls(i, 1);
-      cur_r = walls(i, 2);
-      P(i) = unifcdf(cur_r, l, r) - unifcdf(cur_l, l, r);
+      P(i) = unifcdf(walls(i, 2), l, r) - unifcdf(walls(i, 1), l, r);
     endfor
 
     hi2 = sum(((cnt_in_bucket - n .* P) .^ 2) ./ (n .* P));
-    # TODO: исправить подсчет значения chi2inv
-    res = res + (hi2 < chi2inv(1 - alpha, m - 1));
+    res = res + (hi2 < chi2inv(1 - alpha, m - 1 - 2));
   endfor
+  printf("Равномерное распределение проходит проверку гипотезы о равномерном распределении\n");
+  printf("Для alpha = %d, успешно %d из %d\n", alpha, res, tests);
+endfunction
+
+function res = test_Chi2_2(tests, n, m)
+  res = 0;
+  a = 20;
+  b = 80;
+  alpha = 0.05;
+  for t = 1 : tests    
+    X = sort(unifrnd(a, b, n, 1));
+
+    l = min(X);
+    r = max(X);
+
+    delta = (r - l) / m;
+    cnt_in_bucket = hist(X, m);
+    walls = [];
+    x_coords = [];
+    for i = 1 : m
+      walls(i, 1) = l + delta * (i - 1);
+      walls(i, 2) = l + delta * i;
+      x_coords(i) = (walls(i, 2) + walls(i, 1)) / 2;
+    endfor
+    
+    #Выборочное среднее
+    E = sum(x_coords .* cnt_in_bucket) / n;
+    #Выборочная дисперсия
+    D = sum((x_coords - E) .^ 2 .* cnt_in_bucket) / n;
+    SQRT_D = sqrt(D);
+
+    P = [];
+    for i = 1 : m
+      P(i) = normcdf(walls(i, 2), E, SQRT_D) - normcdf(walls(i, 1), E, SQRT_D);
+    endfor
+
+    hi2 = sum(((cnt_in_bucket - n .* P) .^ 2) ./ (n .* P));
+    res = res + (hi2 < chi2inv(1 - alpha, m - 1 - 2));
+  endfor
+  printf("Равномерное распределение проходит проверку гипотезы о нормальном распределении\n");
   printf("Для alpha = %d, успешно %d из %d\n", alpha, res, tests);
 endfunction
 
 n = 10 ^ 5;
-m = 100;
+m = 10 ^ 2;
 a = 20;
 b = 80;
-alpha = 0.95;
 
-X = sort(unifrnd(a, b, 1, n));
 
 # PART 1
+
+X = sort(unifrnd(a, b, 1, n));
 
 l = min(X);
 r = max(X);
 delta = (r - l) / m;
-x_coords = zeros(m, 1);
-cnt_in_bucket = zeros(m, 1);
-y_coords = zeros(m, 1);
+y_coords = hist(X, m) / (n * delta);
 
+x_coords = [];
 for i = 1 : m
   cur_l = (i - 1) * delta + l;
   cur_r = cur_l + delta;
   x_coords(i) = (cur_r + cur_l) / 2;
-  cnt_in_bucket(i) = sum(X <= cur_r) - sum(X < cur_l);
-  y_coords(i) = cnt_in_bucket(i) / (n * delta);
 endfor
 
 real_y = 1 / (b - a);
@@ -82,7 +109,6 @@ hold on;
 plot([a b], [real_y real_y], "linewidth", 1);
 
 printf("Размер выборки = %d\n", n);
-printf("Границы = [%d; %d]\n", l, r);
 printf("Выбранная длина интервалов = %d\n", delta);
 printf("Количество интервалов = %d\n", m);
 
@@ -91,3 +117,7 @@ printf("\n");
 # PART 2
 
 test_Chi2_1(10 ^ 3, 10 ^ 4, m);
+
+printf("\n");
+
+test_Chi2_2(10 ^ 3, 10 ^ 4, m);
