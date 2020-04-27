9df1bf14a484db0fa385c3ae3be24bb1e023676f
AndreyBocharnikov <a-bocharnikov@bk.ru>
2020-04-28 00:50:53 +0300

    fix, but risk of minimax doens't decrease 1/n

--- hw5/task1.m
+++ hw5/task1.m
@@ -1,5 +1,5 @@
 pkg load statistics
-
+#{
 function count_risks(mu, sigma, n, m)
   C1 = 0.4;
   X = sort(normrnd(mu, sigma, m, n));
@@ -18,6 +18,32 @@ endfunction
 
 mu = 1;
 sigma = 3;
-m = 100;
-count_risks(mu, sigma, 1000, m);
-count_risks(mu, sigma, 10000, m);
+n = 100; # кол-во выборок
+count_risks(mu, sigma, n, 100); 
+count_risks(mu, sigma, n, 10000); # меняем объём выборки 
+#}
+
+function count_risks(mu, sigma, n, m)
+  C1 = 0.4;
+  std1 = std2 = std3 = [];
+  for i = 1 : 10
+    X = sort(normrnd(mu, sigma, m, n));
+    std1(i) = std(mean(X));
+
+    if mod(m, 2) == 1
+      med = X(fix(m / 2) + 1, :);
+    else
+      med = (X(fix(m / 2), :) + X(fix(m / 2) + 1, :)) / 2;
+    endif
+
+    std2(i) = std(med);
+    std3(i) = std((X(1, :) + X(m, :)) / 2);
+  endfor
+  risk1 = mean((std1 - sigma / sqrt(m)) .^ 2)
+  risk2 = mean((std2 - sqrt(pi) * sigma / sqrt(2 * m)) .^ 2)
+  risk3 = mean((std3 - sqrt(C1) * sigma / sqrt(log(m))) .^ 2)
+endfunction
+
+count_risks(1, 3, 100, 100);  
+count_risks(1, 3, 100, 10000);   
+  
