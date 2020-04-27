0e75d27912143156907ab418b2a24b047ddacf2e
AndreyBocharnikov <a-bocharnikov@bk.ru>
2020-04-27 23:01:10 +0300

    bigger test added, but is doesn't work

--- hw5/task1.m
+++ hw5/task1.m
@@ -1,19 +1,23 @@
 pkg load statistics
 
+function count_risks(mu, sigma, n, m)
+  C1 = 0.4;
+  X = sort(normrnd(mu, sigma, m, n));
+  #mean(X)
+  risk1 = mean(mean(X) - sigma ^ 2 / m)
+  if mod(m, 2) == 1
+    med = X(fix(m / 2) + 1, :);
+  else
+    med = (X(fix(m / 2), :) + X(fix(m / 2) + 1, :)) / 2;
+  endif
+
+  risk2 = mean(med - pi * sigma ^ 2 / (2 * m))
+
+  risk3 = mean((X(1, :) + X(m, :)) / 2 - C1 * sigma ^ 2 / log(m))
+endfunction
+
 mu = 1;
 sigma = 3;
-C1 = 0.4;
-n = 100; # кол-во тестов
-m = 100; # кол-во элементов в выборке
-X = sort(normrnd(mu, sigma, n, m));
-
-risk1 = mean(mean(X) - sigma ^ 2 / n)
-if mod(n, 2) == 1
-  med = X(fix(n / 2) + 1, :);
-else
-  med = (X(fix(n / 2), :) + X(fix(n / 2) + 1, :)) / 2;
-endif
-
-risk2 = mean(med - pi * sigma ^ 2 / (2 * n))
-
-risk3 = mean((X(1, :) + X(n, :)) / 2 - C1 * sigma ^ 2 / log(n))
+m = 100;
+count_risks(mu, sigma, 1000, m);
+count_risks(mu, sigma, 10000, m);
