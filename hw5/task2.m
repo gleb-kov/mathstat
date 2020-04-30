322db5b3a0af39fa85f6f4790ca50e4a90845120
gleb-kov <kovg16@gmail.com>
2020-04-30 19:23:15 +0300

    uniform task

--- hw5/task2.m
+++ hw5/task2.m
@@ -1,18 +1,17 @@
 pkg load statistics
 
-function count_risks(a, sigma, n, m)
-  C1 = 0.4;
+function count_risks(a, delta, n, m)
   std1 = std2 = std3 = [];
   for i = 1 : n
-    X = sort(normrnd(a, sigma, m, n));
+    X = sort(normrnd(a - delta / 2, a + delta / 2, m, n));
     med = median(X);
     std1(i) = std(mean(X));
     std2(i) = std(med);
     std3(i) = std((X(1, :) + X(m, :)) / 2);
   endfor
-  risk1 = mean((std1 - sigma / sqrt(m)) .^ 2);
-  risk2 = mean((std2 - sqrt(pi) * sigma / sqrt(2 * m)) .^ 2);
-  risk3 = mean((std3 - sqrt(C1) * sigma / sqrt(log(m))) .^ 2);
+  risk1 = mean((std1 - delta / sqrt(12 * m)) .^ 2);
+  risk2 = mean((std2 - delta / sqrt(4 * m)) .^ 2);
+  risk3 = mean((std3 - delta / sqrt(2 * m * m)) .^ 2);
   printf("Risk1 = %d, risk2 = %d, risk3 = %d\n", risk1, risk2, risk3)
 endfunction
 
