85b471c860cb7b90c5d8a933d608cb3b09f3df6c
gleb-kov <kovg16@gmail.com>
2020-04-30 19:11:20 +0300

    laplase + fix

--- hw5/task3.m
+++ hw5/task3.m
@@ -1,21 +1,21 @@
 pkg load statistics
 
-function count_risks(a, sigma, n, m)
-  C1 = 0.4;
+function count_risks(a, u, n, m)
+  C2 = 0.9;
   std1 = std2 = std3 = [];
   for i = 1 : n
-    X = sort(normrnd(a, sigma, m, n));
+    X = sort(laplace_rnd(m, n));
+    % X += u ???
     med = median(X);
     std1(i) = std(mean(X));
     std2(i) = std(med);
     std3(i) = std((X(1, :) + X(m, :)) / 2);
   endfor
-  risk1 = mean((std1 - sigma / sqrt(m)) .^ 2);
-  risk2 = mean((std2 - sqrt(pi) * sigma / sqrt(2 * m)) .^ 2);
-  risk3 = mean((std3 - sqrt(C1) * sigma / sqrt(log(m))) .^ 2);
+  risk1 = mean((std1 -  u * sqrt(2) / sqrt(m)) .^ 2);
+  risk2 = mean((std2 - u / sqrt(m)) .^ 2);
+  risk3 = mean((std3 - sqrt(C2) * u) .^ 2);
   printf("Risk1 = %d, risk2 = %d, risk3 = %d\n", risk1, risk2, risk3)
 endfunction
 
 count_risks(1, 3, 100, 100);  
-count_risks(1, 3, 100, 10000);   
-  
+count_risks(1, 3, 100, 10000);  
