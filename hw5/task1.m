58071c0ed370ab1754e4fb36f7bf441544c06b36
gleb-kov <kovg16@gmail.com>
2020-05-04 19:02:39 +0300

    rearrange + tex

--- hw5/task1.m
+++ hw5/task1.m
@@ -1,19 +1,19 @@
 pkg load statistics
 
-function count_risks(a, sigma, n, m)
+function count_risks(a, sigma, m, n)
   C1 = 0.4;
-  std1 = std2 = std3 = [];
-  for i = 1 : n
-    X = sort(normrnd(a, sigma, m, n));
-    med = median(X);
-    std1(i) = std(mean(X));
-    std2(i) = std(med);
-    std3(i) = std((X(1, :) + X(m, :)) / 2);
-  endfor
-  risk1 = mean((std1 - sigma / sqrt(m)) .^ 2);
-  risk2 = mean((std2 - sqrt(pi) * sigma / sqrt(2 * m)) .^ 2);
-  risk3 = mean((std3 - sqrt(C1) * sigma / sqrt(log(m))) .^ 2);
-  printf("Risk1 = %d, risk2 = %d, risk3 = %d\n", risk1, risk2, risk3)
+  X = sort(normrnd(a, sigma, n, m));
+  med = median(X);
+  
+  std1_practical = std(mean(X))
+  std1_theoretical = sigma / sqrt(n)
+ 
+  std2 = std(med)
+  std2_theoretical = sqrt(pi) * sigma / sqrt(2 * n)
+  
+  std3 = std((X(1, :) + X(n, :)) / 2)
+  std3_theoretical = sqrt(C1) * sigma / sqrt(log(n))
+  printf('\n');
 endfunction
 
 count_risks(1, 3, 100, 100);
